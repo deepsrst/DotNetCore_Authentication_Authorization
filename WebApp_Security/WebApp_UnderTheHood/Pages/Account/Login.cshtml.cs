4a4cdc16dc33b57d59d79290a18ce7f670c21d4f
Deepak Shrestha <deepak.shrestha@mtradeasia.com>
2023-11-15 01:40:42 +0545

    first commit

---
+++ WebApp_Security/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }  = new Credential();
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            if (!ModelState.IsValid) return Page();
+
+
+            //verify the credential
+            if(Credential.UserName=="admin" && Credential.Password=="admin")
+            {
+                // Creating the security context
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email,"admin@myweb.com"),
+                new Claim("Department","HR"),
+                new Claim("Admin","true")
+
+            };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+
+            }
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+
+}
---
+++ WebApp_Security/WebApp_UnderTheHood/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options => {
+    options.Cookie.Name = "MyCookieAuth";
+    //options.LoginPath = "/Account1/Login";
+});
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("AdminOnly", policy => policy.RequireClaim("Admin"));
+    options.AddPolicy("MustBelongToHRDepartment", policy => policy.RequireClaim("Department", "HR"));
+    options.AddPolicy("HRManager", policy => policy
+                    .RequireClaim("Department", "HR")
+                    .RequireClaim("Manager")              
+                       );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
