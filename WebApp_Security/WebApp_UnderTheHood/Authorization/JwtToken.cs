46f92408ad80c9fa3d6b56cdc848b94480c5f233
Deepak Shrestha <deepak.shrestha@mtradeasia.com>
2023-11-24 01:24:00 +0545

    Token post in weather Forecast -7 completed

---
+++ WebApp_Security/WebApp_UnderTheHood/Authorization/JwtToken.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebApp_UnderTheHood.Authorization
+{
+    public class JwtToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; } = string.Empty;
+
+        [JsonProperty("expires_at")]
+        public DateTime ExpiresAt { get; set; } 
+    }
+}
--- WebApp_Security/WebApp_UnderTheHood/Pages/HRManagement.cshtml.cs
+++ WebApp_Security/WebApp_UnderTheHood/Pages/HRManagement.cshtml.cs
@@ -2,7 +2,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using WebApp_UnderTheHood.Authorization;
 using WebApp_UnderTheHood.DTO;
+using WebApp_UnderTheHood.Pages.Account;
 
 namespace WebApp_UnderTheHood.Pages
 {
@@ -22,6 +28,21 @@ namespace WebApp_UnderTheHood.Pages
         public async Task OnGetAsync()
         {
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+
+            // Post auth credential to get access_token
+            var res = await httpClient.PostAsJsonAsync("auth", new Credential
+            {
+                UserName = "admin",
+                Password = "admin"
+            });
+            res.EnsureSuccessStatusCode();
+            string strJwt = await res.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
+
+
+            //send obtained token along with header
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken ?? string.Empty);
+
             weatherForecastItems =await httpClient.GetFromJsonAsync<List<WeatherForecasetDTO>>("WeatherForecast")?? new List<WeatherForecasetDTO>();
         }
     }
