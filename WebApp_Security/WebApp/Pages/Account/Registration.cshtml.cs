e8f07cf191663da8edae42c833b4f13078130f84
Deepak Shrestha <deepak.shrestha@mtradeasia.com>
2023-11-27 00:22:40 +0545

    Identity user created, login to do

---
+++ WebApp_Security/WebApp/Pages/Account/Registration.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Pages.Account
+{
+    public class RegistrationModel : PageModel
+    {
+
+        public RegistrationModel(UserManager<IdentityUser> userManager)
+        {
+            UserManager = userManager;
+        }
+        [BindProperty]
+        public RegistrationViewModel RegistrationViewModel { get; set; } = new RegistrationViewModel();
+        public UserManager<IdentityUser> UserManager { get; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = new IdentityUser()
+            {
+                Email = RegistrationViewModel.Email,
+                UserName = RegistrationViewModel.Email
+            };
+
+            var result = await UserManager.CreateAsync(user, RegistrationViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return Page();
+            }
+        }
+    }
+
+    public class RegistrationViewModel
+    {
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- WebApp_Security/WebApp/Program.cs
+++ WebApp_Security/WebApp/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
 
@@ -9,6 +10,24 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Lockout.MaxFailedAccessAttempts= 5; ;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);   
+    options.User.RequireUniqueEmail = true;
+
+})
+  .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+   
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
